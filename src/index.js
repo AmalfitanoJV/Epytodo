49170fcfbd8365ef2303d58017c6aa532976e49d
Jean-Vincent Amalfitano <JV@localhost.localdomain>
2021-03-26 06:59:31 -0400

    add + modid: package-lock.json added and modif index.json to start the server

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mysql = require('mysql2');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+
+});
