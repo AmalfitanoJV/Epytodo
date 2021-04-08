ba9bb9af09be4a73de1f4fdb07fd6af1c4ef819b
Jean-Vincent Amalfitano <JV@localhost.localdomain>
2021-04-08 09:58:09 -0400

    fix: db running

--- src/config/db.js
+++ src/config/db.js
@@ -4,13 +4,23 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Database connected.");
-});
-
+async function tableservice(req, res, next) {
+    connection.execute(
+        'SELECT * `client` WHERE `nom` = ?',
+        ['Rick C-137'],
+        function(err, results, fields) {
+            if (err) {
+                console.log(err.toString());
+            } else {
+                console.log(results);
+                console.log(fields);
+            }
+        }
+    );
+    res.send("ok mec");
+};
 module.exports = connection;
