db52e45b9ea67ec3b9df31147afae203658bd3e4
Alexis <alexis.camra@epitech.eu>
2021-04-09 13:26:14 +0200

    modified: sql file

---
+++ epytodo.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS epytodo;
+CREATE DATABASE epytodo;
+use epytodo;
+CREATE TABLE user(
+    id  INT NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    firstname varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    primary key (id));
+CREATE TABLE todos(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    due_time TIMESTAMP,
+    user_id INT NOT NULL,
+    status ENUM('not started', 'todo', 'in progress', 'done') DEFAULT 'not started',
+    primary key (id));
