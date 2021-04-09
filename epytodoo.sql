06503bf10bb5af304d66587d3451a324ef0a2bd2
Alexis <alexis.camra@epitech.eu>
2021-04-09 10:18:11 +0200

    .sql

--- epytodoo.sql
+++ epytodoo.sql
@@ -2,20 +2,20 @@ create database epytodo;
 use epytodo;
 CREATE TABLE users
 (
-    id INT PRIMARY KEY NOT NULL,
-    email VARCHAR(255),
-    password VARCHAR(255),
+    id int,
+    email varchar(255),
+    password varchar(255),
     created_at TIMESTAMP,
-    firstname VARCHAR(100),
-    name VARCHAR(100),
+    firstname varchar(100),
+    name varchar(100),
 );
 CREATE TABLE todos
 (
-    id INT PRIMARY KEY NOT NULL,
-    title VARCHAR(255),
-    description VARCHAR(255),
+    id int,
+    title varchar(255),
+    description varchar(255),
     creatd_at TIMESTAMP,
     due_time TIMESTAMP,
-    user_id INT PRIMARY KEY NOT NULL,
-    status VARCHAR(255),
-)
+    user_id int,
+    status varchar(255),
+);
