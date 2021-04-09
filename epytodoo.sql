32222c94765594846f553d7503e69104f56b36fb
Alexis <alexis.camra@epitech.eu>
2021-04-09 11:23:26 +0200

    modified: sql script

--- epytodoo.sql
+++ epytodoo.sql
@@ -1,22 +1,18 @@
-DROP DATABASE epytodo;
-CREATE DATATBASE epytodo;
+ DROP DATABASE IF EXISTS epytodo;
+CREATE DATABASE epytodo;
 use epytodo;
-CREATE TABLE users
-(
+CREATE TABLE user(
     id int,
     email varchar(255),
     password varchar(255),
     created_at TIMESTAMP,
     firstname varchar(100),
-    name varchar(100),
-);
-CREATE TABLE todos
-(
+    name varchar(100));
+CREATE TABLE todos(
     id int,
     title varchar(255),
     description varchar(255),
     creatd_at TIMESTAMP,
     due_time TIMESTAMP,
     user_id int,
-    status varchar(255),
-);
+    status varchar(255));
