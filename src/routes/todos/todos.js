0b9c1c5c873cbf4c8ab00f358cb1457ea79da303
Jean-Vincent Amalfitano <JV@localhost.localdomain>
2021-04-09 12:13:36 -0400

    modi: /todo ok have to put error gestions

--- src/routes/todos/todos.js
+++ src/routes/todos/todos.js
@@ -18,19 +18,19 @@ const todo = async (req, res) => {
     const due_time = req.body.due_time;
     const status = req.body.status;
 
-    var token = jwt.sign({ "title": "value", "description": "due_time", "user_id": "value", "status": "value"}, process.env.SECRET);
-    db.query("INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)", [title, description, due_time, user_id, status], (err, rows, fields) => {
+    console.log(`User $}`);
+
+    db.query("INSERT INTO todos (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)", [title, description, due_time, user_id, status], (err, rows, fields) => {
         if (err) {
             console.log(err.toString());
-            res.status(500).send({msg : " creation error " + err});
+            res.status(500).send({msg : " creation " + err});
         } else {
             console.log(fields);
             console.log(rows);
             console.log(process.env.SECRET);
-            res.json({ "token": token});
         }
     });
-};
+}
 
 module.exports = {
     view_all,
