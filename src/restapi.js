fdb8383aad31d39a8a2fdf569c36db54e8aa7563
Jean-Vincent Amalfitano <JV@localhost.localdomain>
2021-04-09 10:56:23 -0400

    modif: /register ok /user/id => delete creation

--- src/restapi.js
+++ src/restapi.js
@@ -16,7 +16,11 @@ routeur.get("/todo", todoservice.view_all);
 routeur.get("/todo/:id", todoservice.view_user);
 
 routeur.get("/user", userservice.all_users_info);
-routeur.get("/user/:id" || "/user/:email", userservice.user_info);
+routeur.get("/user/:id", userservice.user_info);
+routeur.get("/user/:email", userservice.user_info);
 routeur.get("/user/todo", userservice.user_todo);
+
+//delete data
+routeur.delete("/user/:id", userservice.user_delete);
 
 module.exports = routeur;
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,10 +1,4 @@
-const db = require("../../config/db");
-
-const User = function(user) {
-    this.email = user.email;
-    this.name = user.name;
-    this.active = user.active;
-};
+const db = require("../../config/db").connection;
 
 const all_users_info = async (req, res) => {
     return res.send("all_users_info\n");
@@ -18,8 +12,20 @@ const user_info = async (req, res) => {
     return res.send("user_info\n");
 }
 
+const user_delete = async (req, res) => {
+
+    db.query ("DELETE FROM user WHERE id = ?"), [req.params.id], (err, res) => {
+        if (err) {
+            console.log(err.toString());
+        } else {
+            console.log("user id is " + req.params.id + " deleted");
+        }
+    };
+}
+
 module.exports = {
     all_users_info,
     user_todo,
-    user_info
+    user_info,
+    user_delete
 }
