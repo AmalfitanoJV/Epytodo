7ebacb2fccaad9a46c813d41e4779790db338296
Alexis <alexis.camra@epitech.eu>
2021-04-09 17:19:52 +0200

    todo create

--- src/routes/todos/todos.js
+++ src/routes/todos/todos.js
@@ -9,7 +9,15 @@ const view_user = async (req, res) => {
 }
 
 const todo = async (req, res) => {
+    const user_id = req.body.user_id;
+    const description = req.body.description;
+    const title = req.body.title;
+    const due_time = req.body.due_time;
     db.query("INSERT INTO todo (title, description, due_time, user_id) VALUES (?, ?, ?, ?)", [title, description, due_time, user_id]);
+    if (err) {
+        console.log(err.toString());
+        res.status(500).send({msg : " creation error " + err});
+    }
 }
 
 module.exports = {
